8fb5fcc8718448eea2fb3f799d6df5b7f1231c6a
Roland Kullet <renaud.bellec.3@gmail.com>
2016-09-29 14:33:20 +0200

    _contextual-menu.scss : added dropdown menu

---
+++ public/js/all.js
@@ -0,0 +1,15 @@
+$(function ($) {
+    $(".list__collapsable").collapse();
+
+    $(".list__clickable").on("click", function(ev) {
+        $(this).next().collapse("toggle");
+    });
+
+    $(".dropdown-menu").dropdown();
+    $(".dropdown").on("show.bs.dropdown", function () {
+        $(this).find(".dropdown-menu").slideDown(200);
+    });
+    $(".dropdown").on("hide.bs.dropdown", function () {
+        $(this).find(".dropdown-menu").slideUp(200);
+    });
+});
--- public/js/lists.js
+++
@@ -1,7 +0,0 @@
-$(function ($) {
-    $(".list__collapsable").collapse();
-
-    $(".list__clickable").on("click", function(ev) {
-        $(this).next().collapse("toggle");
-    });
-});
