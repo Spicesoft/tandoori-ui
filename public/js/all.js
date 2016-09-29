78b18a46b1cf14759766e8b78e9408624a8755fb
Roland Kullet <renaud.bellec.3@gmail.com>
2016-09-29 15:18:27 +0200

    Contextual menus : improvements

--- public/js/all.js
+++ public/js/all.js
@@ -7,9 +7,9 @@ $(function ($) {
 
     $(".dropdown-menu").dropdown();
     $(".dropdown").on("show.bs.dropdown", function () {
-        $(this).find(".dropdown-menu").slideDown(200);
+        $(this).find(".dropdown-menu").slideDown(150);
     });
     $(".dropdown").on("hide.bs.dropdown", function () {
-        $(this).find(".dropdown-menu").slideUp(200);
+        $(this).find(".dropdown-menu").slideUp(150);
     });
 });
