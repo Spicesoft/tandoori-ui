72e1d9f78827336c5618fa3a2977b7a09db60b39
Roland Kullet <renaud.bellec.3@gmail.com>
2017-06-14 15:28:27 +0200

    made tui ready for publication

--- public/js/sideMenu.dist.js
+++ public/js/sideMenu.dist.js
@@ -1,3 +1,4 @@
+var tandooriUi =
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,43 @@
 /* eslint-env node */
 const path = require("path");
 
+function getExternals(env) {
+    if (env === "prod") {
+        return [
+            "react",
+            "react-dom"
+        ];
+    }
+    return;
+}
 
-module.exports = {
-    entry: {
-        navbar: "./static/js/navbar/App.jsx",
-        sideMenu: "./static/js/side-menu/App.jsx"
-    },
-    output: {
-        path: path.resolve(__dirname, "public/js"),
-        filename: "[name].dist.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                use: [{
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["es2015", "react"],
-                        plugins: ["transform-object-rest-spread"]
-                    }
-                }]
-            }
-        ]
-    },
-    devtool: "source-map"
+
+module.exports = function (env) {
+    return {
+        entry: {
+            navbar: "./static/js/navbar/App.jsx",
+            sideMenu: "./static/js/side-menu/App.jsx"
+        },
+        output: {
+            path: path.resolve(__dirname, "public/js"),
+            filename: "[name].dist.js",
+            library: "tandooriUi"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    use: [{
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["es2015", "react"],
+                            plugins: ["transform-object-rest-spread"]
+                        }
+                    }]
+                }
+            ]
+        },
+        devtool: "source-map",
+        externals: getExternals(env)
+    };
 };
