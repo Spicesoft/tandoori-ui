6b432b7179a438f167f0eddb113b0daec34c1c78
Roland Kullet <renaud.bellec.3@gmail.com>
2017-06-16 12:46:48 +0200

    exported Dropdown & Loader

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,5 @@
 import Navbar from "./navbar/ui/Navbar.jsx";
+import Dropdown from "./shared/Dropdown.js";
+import Loader from "./shared/Loader.jsx";
 
-export {Navbar};
+export {Navbar, Dropdown, Loader};
