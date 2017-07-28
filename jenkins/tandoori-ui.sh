b05750279ab28b5640b6b765fa20540375fdf223
Roland Kullet <renaud.bellec.3@gmail.com>
2017-07-28 18:21:26 +0200

    fixed codecov

--- jenkins/tandoori-ui.sh
+++ jenkins/tandoori-ui.sh
@@ -4,8 +4,5 @@ npm install
 ## tests and coverage
 npm run test
 
-## install codecov
-bash <(curl -s https://codecov.io/bash) -t 3ff2073c-d9e0-4ab6-92d3-3ff38b68d9de
-
-# ------ COVERAGE UPLOAD ------
-codecov --token=3ff2073c-d9e0-4ab6-92d3-3ff38b68d9de --file static/js/coverage/lcov.info
+## codecov upload
+bash <(curl -s https://codecov.io/bash) -t 3ff2073c-d9e0-4ab6-92d3-3ff38b68d9de -f static/js/coverage/lcov.info
