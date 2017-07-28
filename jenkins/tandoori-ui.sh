8af7f07c22cb399c8f34154c88566c012b165d4c
Roland Kullet <renaud.bellec.3@gmail.com>
2017-07-28 17:40:23 +0200

    jenkins ?

---
+++ jenkins/tandoori-ui.sh
@@ -0,0 +1,9 @@
+## JS ENV
+npm install
+
+## tests and coverage
+npm run test
+
+
+# ------ COVERAGE UPLOAD ------
+codecov --token=3ff2073c-d9e0-4ab6-92d3-3ff38b68d9de --file coverage.xml static/js/coverage/lcov.info
