b77afe07f0c0b118ddd78042c757f2e4ade2efbb
Roland Kullet <renaud.bellec.3@gmail.com>
2017-07-28 18:18:05 +0200

    updated codecov install script

--- jenkins/tandoori-ui.sh
+++ jenkins/tandoori-ui.sh
@@ -4,6 +4,8 @@ npm install
 ## tests and coverage
 npm run test
 
+## install codecov
+bash <(curl -s https://codecov.io/bash) -t 3ff2073c-d9e0-4ab6-92d3-3ff38b68d9de
 
 # ------ COVERAGE UPLOAD ------
 codecov --token=3ff2073c-d9e0-4ab6-92d3-3ff38b68d9de --file static/js/coverage/lcov.info
