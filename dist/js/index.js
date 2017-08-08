6ed412bf6640466ddc0767e094df56e58f0c6beb
Roland Kullet <renaud.bellec.3@gmail.com>
2017-08-08 15:02:45 +0200

    index.js

---
+++ dist/js/index.js
@@ -0,0 +1,5 @@
+import Navbar from "./navbar/ui/Navbar.jsx";
+import Dropdown from "./shared/Dropdown.jsx";
+import Loader from "./shared/Loader.jsx";
+
+export {Navbar, Dropdown, Loader};
--- static/js/index.js
+++
@@ -1,5 +0,0 @@
-import Navbar from "root/dist/js/navbar/ui/Navbar.jsx";
-import Dropdown from "root/dist/js/shared/Dropdown.jsx";
-import Loader from "root/dist/js/shared/Loader.jsx";
-
-export {Navbar, Dropdown, Loader};
